b59d17882520904e4846fff8c54890596b420b37
zhangk6 <zhangk6@knownsec.com>
2023-10-11 17:15:37 +0800

    v3

---
+++ demos/demo.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+from pyscanasdk.v3 import ModerateClient
+import uuid
+
+app_id = "xxxx"
+secret_key = "xxxx"
+text_business_id = 'xxxx'
+img_business_id = 'xxxx'
+audio_business_id = 'xxxx'
+video_business_id = 'xxxx'
+doc_business_id = 'xxxx'
+
+def gen_sign_by_url(url):
+    mc = ModerateClient(app_id=app_id,  # 得到的appid
+                        secret_key=secret_key,  # 得到的秘钥
+                        doc_business_id=doc_business_id)  # 文档的业务id
+
+    sign = mc.gen_signature_by_url(url)
+    return sign
+
+
+def gen_sign(business_id, timestamp, nonce):
+    mc = ModerateClient(app_id=app_id,  # 得到的appid
+                        secret_key=secret_key,  # 得到的秘钥
+                        doc_business_id=doc_business_id)  # 文档的业务id
+    # 生成signature  business_id为当前业务类型的id     timestamp：时间戳  nonce 随机数
+    sign = mc.gen_signature(business_id=business_id, timestamp=timestamp, nonce=nonce)
+    return sign
+
+def text_test():
+    """
+    文本检测 示例
+    """
+    mc = ModerateClient(app_id=app_id,  # 得到的appid
+                        secret_key=secret_key,  # 得到的秘钥
+                        text_business_id=text_business_id)  # 文本的业务id
+    content = [
+        {"contentId": uuid.uuid4().__str__(), "data": "待检测内容1"},
+        {"contentId": uuid.uuid4().__str__(), "data": "待检测内容2"},
+    ]
+    resp = mc.text(content=content, ac=True)  # ac默认值为False 同步     为True时异步
+    print(resp)
+    # 生成signature  business_id为当前业务类型的id     timestamp：时间戳  nonce 随机数
+    sign = mc.gen_signature(business_id=text_business_id, timestamp=1615646461100, nonce=1)
+    print(sign)
+
+
+def img_test():
+    """
+    图片检测 示例
+    """
+    mc = ModerateClient(app_id=app_id,  # 得到的appid
+                        secret_key=secret_key,  # 得到的秘钥
+                        img_business_id=img_business_id)  # 图片的业务id
+    content = [
+        {
+            "contentId": "测试回调",
+            "data": "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png",  # 图片链接
+            "type": 1  # 调用类型，1图片url，2图片base64
+        },
+        {
+            "contentId": "测试回调2",
+            "data": "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png",  # 图片链接
+            "type": 1  # 调用类型，1图片url，2图片base64
+        },
+    ]
+    resp = mc.image(content=content, ac=True)  # ac默认值为False 同步     为True时异步
+    print(resp)
+    # 生成signature  business_id为当前业务类型的id     timestamp：时间戳  nonce 随机数
+    sign = mc.gen_signature(business_id=img_business_id, timestamp=1615646461100, nonce=1)
+    print(sign)
+
+
+def video_test():
+    """
+    视频检测 示例
+    """
+    mc = ModerateClient(app_id=app_id,  # 得到的appid
+                        secret_key=secret_key,  # 得到的秘钥
+                        video_business_id=video_business_id)  # 视频的业务id
+    content = 'http://example.mp4'  # 待检测视频链接
+    resp = mc.video(content=content)
+    print(resp)
+    # 生成signature  business_id为当前业务类型的id     timestamp：时间戳  nonce 随机数
+    sign = mc.gen_signature(business_id=video_business_id, timestamp=1615646461100, nonce=1)
+    print(sign)
+
+
+def audio_test():
+    """
+    音频检测 示例
+    """
+    mc = ModerateClient(app_id=app_id,  # 得到的appid
+                        secret_key=secret_key,  # 得到的秘钥
+                        audio_business_id=audio_business_id)  # 音频的业务id
+    content = 'http://example.mp4'  # 待检测音频链接
+    resp = mc.audio(content=content)
+    print(resp)
+    # 生成signature  business_id为当前业务类型的id     timestamp：时间戳  nonce 随机数
+    sign = mc.gen_signature(business_id=audio_business_id, timestamp=1615646461100, nonce=1)
+    print(sign)
+
+
+def doc_test():
+    """
+    文档检测 示例
+    """
+    mc = ModerateClient(app_id=app_id,  # 得到的appid
+                        secret_key=secret_key,  # 得到的秘钥
+                        doc_business_id=doc_business_id)  # 文档的业务id
+    content = 'http://example.docx'  # 待检测文档链接
+    resp = mc.doc(content=content)
+    print(resp)
+    # 生成signature  business_id为当前业务类型的id     timestamp：时间戳  nonce 随机数
+    sign = mc.gen_signature(business_id=doc_business_id, timestamp=1615646461100, nonce=1)
+    print(sign)
+
+
+
+
+if __name__ == "__main__":
+    # business_id, timestamp, nonce = '111111111', 1611111111111, 1
+    # print(gen_sign(business_id, timestamp, nonce))
+    #
+    #
+    # url = 'https://xxxx.xxx?businessId=111111111&nonce=1&timestamp=1611111111111'
+    # print(gen_sign_by_url(url))
+    # pass
+    mc = ModerateClient(img_business_id='1800097935845781514', secret_key='a2f175a8-5373-11ed-9949-0242ac12000e',
+                        app_id='62ac786311b92177337a933b'
+                        )
+    content = [
+        {
+            "contentId": "测试回调",
+            "data": "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png",  # 图片链接
+            "type": 1  # 调用类型，1图片url，2图片base64
+        }
+
+    ]
+    res = mc.image(content=content, extra='123')
+    print(res)
---
+++ src/pyscanasdk/v3.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+import requests
+import hashlib
+import time
+from urllib.parse import urlparse, parse_qs
+
+
+# https://packaging.python.org/en/latest/tutorials/packaging-projects/#creating-the-package-files
+class ModerateClient(object):
+    def __init__(self, app_id, secret_key, text_business_id='', img_business_id='', audio_business_id='',
+                 video_business_id='', doc_business_id=''):
+        """
+        :param app_id: 获取来的appid
+        :param secret_key: 获取到的和appid对应的secret_key
+        :param text_business_id: 文本审核的业务id
+        :return:
+        """
+        self.app_id = app_id
+        self.secret_key = secret_key
+        self.text_business_id = text_business_id
+        self.img_business_id = img_business_id
+        self.audio_business_id = audio_business_id
+        self.video_business_id = video_business_id
+        self.doc_business_id = doc_business_id
+
+    def text(self, content, ac=False, extra="", timeout=10):
+        """
+        文本审核的接口
+        :param content: 待审核的文本内容
+        :param ac: False同步/True异步
+        :param timeout: 超时时间，单位秒
+        :param extra: 透传字段
+        :return:
+        """
+        if not self.text_business_id:
+            return {"msg": "文本business_id不能为空!"}
+        url = "https://newkmsapi.qixincha.com/kms-open/v3/text/sync"
+        async_url = "https://newkmsapi.qixincha.com/kms-open/v3/text/async"
+        moderation_url = async_url if ac else url
+        data = {
+            "appId": self.app_id,
+            "secretKey": self.secret_key,
+            "businessId": self.text_business_id,
+            "text": content,
+            "extra": extra
+        }
+        resp = requests.post(url=moderation_url, json=data, timeout=timeout)
+        return resp.json()
+
+    def image(self, content, ac=False, extra="", timeout=10):
+        """
+        图片审核的接口
+        :param content: 待审核的图片
+        :param ac: False同步/True异步
+        :param timeout: 超时时间，单位秒
+        :param extra: 透传字段
+        :return:
+        """
+        if not self.img_business_id:
+            return {"msg": "图片business_id不能为空!"}
+        url = "https://newkmsapi.qixincha.com/kms-open/v3/image/sync"
+        async_url = "https://newkmsapi.qixincha.com/kms-open/v3/image/async"
+        moderation_url = async_url if ac else url
+        data = {
+            "appId": self.app_id,
+            "secretKey": self.secret_key,
+            "businessId": self.img_business_id,
+            "images": content,
+            "extra": extra
+        }
+        resp = requests.post(url=moderation_url, json=data, timeout=timeout)
+        return resp.json()
+
+    def audio(self, content, extra="", timeout=10):
+        """
+        图片审核的接口
+        :param content: 待审核的音频
+        :param timeout: 超时时间，单位秒
+        :param extra: 透传字段
+        :return:
+        """
+        if not self.audio_business_id:
+            return {"msg": "音频business_id不能为空!"}
+        async_url = "https://newkmsapi.qixincha.com/kms-open/v3/audio/async"
+        moderation_url = async_url
+        data = {
+            "appId": self.app_id,
+            "secretKey": self.secret_key,
+            "businessId": self.audio_business_id,
+            "url": content,
+            "extra": extra
+        }
+        resp = requests.post(url=moderation_url, json=data, timeout=timeout)
+        return resp.json()
+
+    def video(self, content, extra="", timeout=10):
+        """
+        图片审核的接口
+        :param content: 待审核的视频
+        :param timeout: 超时时间，单位秒
+        :param extra: 透传字段
+        :return:
+        """
+        if not self.video_business_id:
+            return {"msg": "视频business_id不能为空!"}
+        async_url = "https://newkmsapi.qixincha.com/kms-open/v3/video/async"
+        moderation_url = async_url
+        data = {
+            "appId": self.app_id,
+            "secretKey": self.secret_key,
+            "businessId": self.video_business_id,
+            "url": content,
+            "extra": extra
+        }
+        resp = requests.post(url=moderation_url, json=data, timeout=timeout)
+        return resp.json()
+
+    def doc(self, content, extra="", timeout=10):
+        """
+        文档审核的接口
+        :param content: 待审核的文档
+        :param timeout: 超时时间，单位秒
+        :param extra: 透传字段
+        :return:
+        """
+        if not self.doc_business_id:
+            return {"msg": "文档business_id不能为空!"}
+        async_url = "https://newkmsapi.qixincha.com/kms-open/v3/doc/async"
+        moderation_url = async_url
+        data = {
+            "appId": self.app_id,
+            "secretKey": self.secret_key,
+            "businessId": self.doc_business_id,
+            "url": content,
+            "extra": extra
+        }
+        resp = requests.post(url=moderation_url, json=data, timeout=timeout)
+        return resp.json()
+
+    def gen_signature(self, business_id, timestamp, nonce):
+        """生成签名信息
+        Args: business_id 业务id
+        timestamp 时间戳
+        nonce 随机数 不要取0
+        Returns: 参数签名md5值
+        """
+        if not any([business_id, timestamp, nonce]):
+            print('businessId,timestamp,nonce不能为空！')
+            return ''
+        params = {"nonce": nonce, "timestamp": timestamp, 'businessId': business_id}
+        buff = ""
+        for k in sorted(params.keys()):
+            buff += str(k) + str(params[k])
+        buff += self.secret_key
+        return hashlib.md5(buff.encode('utf8')).hexdigest()
+
+    def gen_signature_by_url(self, url):
+        """生成签名信息
+        Args: params url 回调地址
+        Returns: 参数签名md5值
+        """
+        query_dict = parse_qs(urlparse(url).query)
+        business_id = query_dict.get('businessId', [])
+        nonce = query_dict.get('nonce', '')
+        timestamp = query_dict.get('timestamp', '')
+        if not any([business_id, nonce, timestamp]):
+            print('从query获取参数失败')
+            return ''
+        business_id = business_id[0]
+        nonce = nonce[0]
+        timestamp = timestamp[0]
+        params = {"nonce": nonce, "timestamp": timestamp, 'businessId': business_id}
+        buff = ""
+        for k in sorted(params.keys()):
+            buff += str(k) + str(params[k])
+        buff += self.secret_key
+        return hashlib.md5(buff.encode('utf8')).hexdigest()
